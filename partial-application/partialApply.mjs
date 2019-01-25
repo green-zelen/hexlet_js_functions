93f9ab018971b974dd228ed146f53940585ceef8
Mariia Perlovskaia <marigreenzelen@gmail.com>
2019-01-25 19:49:05 +0100

    Частичное применение функций – подробный разбор функции.

--- partial-application/partialApply.mjs
+++ partial-application/partialApply.mjs
@@ -1,12 +1,12 @@
-const sum = (a, b) => a * b;
+const sum = (a, b) => a + b;
 const partialApply = (fn, arg2) => (arg1) => fn(arg1, arg2);
 const resFn = partialApply(sum, 5);
 console.log(resFn(3));
 
-// const partialApply2 = (fn, arg2) => {
-//     (arg1) => {
-//         fn = (arg1, arg2);
-//     }
-// }
-// const resFn2 = partialApply2(sum, 5);
-// console.log(resFn2(3));
+const partialApply2 = (fn, arg2) => {
+    return (arg1) => {
+        return fn(arg1, arg2);
+    }
+}
+const resFn2 = partialApply2(sum, 5);
+console.log(resFn2(3));
